130255de9d1f045542a2ffb3e452d1adb6eba132
Abhishek <wayforward@Abhisheks-MacBook-Pro.local>
2018-05-30 02:33:34 +0530

    mongodb nodejs connection and fetching hotels data from hotels collection

--- api/controllers/hotels.controller.js
+++ api/controllers/hotels.controller.js
@@ -1,18 +1,37 @@
 var hotelController = {};
 var hotelData = require('../data/hoteldata.json');
+var dbconn    = require('../data/dbconnection.js');
+var ObjectId = require('mongodb').ObjectID
 hotelController.getHotel = function(req,res){
-  console.log("Inside getHotel ");
-  res
-    .status(200)
-    .json(hotelData);
+  var db = dbconn.get();
+  db.collection('hotels').find().toArray(function(err,result){
+    if(err){
+      console.log("EEEEEEEE");
+    }
+    else{
+      console.log("the result is ",result);
+      res
+        .status(200)
+        .json(result);
+    }
+  });
 }
 hotelController.getOneHotel = function(req,res){
   console.log("Inside getHotel by params ",req.params.hotelId);
   var hotelId = req.params.hotelId;
-  var thisHotel = hotelData[hotelId];
-  res
-    .status(200)
-    .json(thisHotel);
+  var db = dbconn.get();
+  db.collection('hotels').findOne({_id : ObjectId(hotelId)},function(err,result){
+    if(err){
+      console.log("EE");
+    }
+    else{
+      console.log("the result is ",result);
+      res
+        .status(200)
+        .json(result);
+    }
+  });
+
 }
 hotelController.addOneHotel = function(req,res){
   console.log("Inside addOneHotel by params ",req.body);
